299c03109a5318a4556e052bedfee6c7a803b689
jewelran <mdjewelrana6487@gmail.com>
2021-05-18 15:23:08 +0600

    upload imginfo done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const  bodyParser = require('body-parser')
 require('dotenv').config()
 const cors = require("cors")
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5500
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,29 +12,30 @@ app.use(cors())
 // console.log( process.env.DB_USER,process.env.DB_PASS);
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ckgfu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5lxzf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const foodsCollection = client.db("foods").collection("allFoodsStore");
-  console.log("data base connected");
-
-  app.post('/addFoods', (req, res) => {
-    const foods = req.body;
-    console.log(foods);
-    foodsCollection.insertOne(foods)
-    .then(response => {
-      console.log(response);
-      // res.send(response)
+  const foodsOnlineCollection = client.db("foodsOnline").collection("storeFoods");
+  
+  app.post('/addFoods' , (req, res) => {
+    const foodsInfo = req.body;
+    foodsOnlineCollection.insertOne(foodsInfo)
+    .then(res => {
+      console.log(res);
     })
+
+    console.log(foodsInfo);
   })
 
-  app.get('/foods',(req, res) => {
-    res.send("data get success")
+  app.get('/allFoods', (req, res) => {
+      foodsOnlineCollection.find()
+      .toArray((error, document) => {
+        res.send(document)
+      })
   })
+  console.log("data connected");
 
 });
-
-
 
 
 
@@ -45,3 +46,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
